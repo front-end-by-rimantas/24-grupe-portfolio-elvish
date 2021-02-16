f94eec5a880e307a017dcbe8f3be4f0234a5c67a
Paulius Jarutis <76951786+PJarrr@users.noreply.github.com>
2021-02-16 16:44:21 +0200

    worc section js data info

---
+++ js/data/our-work-data.js
@@ -0,0 +1,51 @@
+const data = [
+{
+    // frame
+    id: 1,
+    categories: ["seo", "webdesign"],
+    title: "Creative & art",
+    description: ["Illustrations"],
+    img: "../img/work/poster-frame.jpg", 
+},
+{
+    // cup
+    id: 2,
+    categories: ["seo", "webdesign"],
+    title: "Locked steel gate",
+    description: ["Graphics", "UI elements"],
+    img: "../img/work/cup.jpg", 
+},
+{
+    // book
+    id: 3,
+    categories: ["seo", "work"],
+    title: "Mac sunglasses",
+    description: ["Illustrations", "Graphics"],
+    img: "../img/work/book.jpg", 
+},
+{
+    // notepad
+    id: 4,
+    category: ["webdesign", "wordpress"],
+    title: "Studio & Art",
+    description: ["UI elements","Icons"],
+    img: "../img/work/note-pad.jpg", 
+},
+{
+    // vinyl
+    id: 5,
+    category: ["work"],
+    title: "Open Imagination",
+    description: ["media", "icons"],
+    img: "../img/work/vinyl.jpg", 
+},
+{
+    // opened book
+    id: 6,
+    category: ["Wordpress"],
+    title: "Backpack Contents",
+    description: "",
+    img: "../img/work/opened-book.jpg", 
+},
+
+]
