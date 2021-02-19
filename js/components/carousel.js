4effedbc0f4645a1659725bfe42b36c00254cca7
Vadim <vadim.mozeiko@gmail.com>
2021-02-19 10:15:53 +0200

    Blog image carousel #59

--- js/components/carousel.js
+++ js/components/carousel.js
@@ -1,9 +1,12 @@
-let myIndex = 0;
+//image carousel
 
+let myIndex = 0;
+let timeout;
 function carousel() {
-  const images = document.getElementsByClassName("carousel-img");
-  for (let i = 0; i < images.length; i++) {
-    images[i].classList.remove('slide');
+  const images = document.querySelectorAll('.carousel-img');
+
+  for (const list of images) {
+    list.classList.remove('slide');
   }
   myIndex++;
   if (myIndex > images.length) {
@@ -11,7 +14,42 @@ function carousel() {
   } 
     images[myIndex-1].classList.add('slide');
 
-  setTimeout(carousel, 5000);
+ timeout = setTimeout(carousel, 5000);
+}
+
+function stopTimeout() {
+    clearTimeout(timeout)
+}
+
+
+// image slide on click
+
+let slideIndex = 1;
+slideShow(slideIndex);
+
+const prvButton = document.querySelector('.slider-prv');
+const nxtButton = document.querySelector('.slider-nxt');
+
+prvButton.addEventListener('click', () => {
+  stopTimeout()
+  slideShow(slideIndex += -1)});
+nxtButton.addEventListener('click', () => {
+  stopTimeout()
+  slideShow(slideIndex += 1)
+});
+
+function slideShow(n) {
+  const images = document.querySelectorAll('.carousel-img');
+  if (n > images.length) {
+    slideIndex = 1
+  }
+  if (n < 1) {
+    slideIndex = images.length
+  }
+  for (const list of images) {
+    list.classList.remove('slide');
+  }
+  images[slideIndex-1].classList.add('slide');
 }
 
 export {carousel}
