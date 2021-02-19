35033c7ab8a3f42d79c644a572b6baf9ab189c39
Gilvydas <gilvydas.kuprevicius@gmail.com>
2021-02-19 14:11:59 +0200

    services initial js

---
+++ js/components/Services.js
@@ -0,0 +1,22 @@
+class Services {
+    constructor(selector, data) {
+        this.data = data;
+        this.selector = selector;
+        this.Dom = '';
+        this.init()
+    }
+
+    init() {
+        this.renderServices()
+    }
+
+    generateServices() {
+
+    }
+
+    renderServices() {
+        const
+    }
+}
+
+export { Services }
--- js/data/services.js
+++ js/data/services.js
@@ -1,46 +1,46 @@
 const topServices = [
-    {
-        icon: 'mbri-database',
-        heading: 'Graphic Design',
-        text: 'The standart chunk of Lorem Ipsum used since the is reproduced \
+        {
+                icon: 'mbri-database',
+                heading: 'Graphic Design',
+                text: 'The standart chunk of Lorem Ipsum used since the is reproduced \
         below for those interested'
-    },
-    {
-        icon: 'mbri-website-theme',
-        heading: 'Unlimited Color',
-        text: 'The standart chunk of Lorem Ipsum used since the is reproduced \
+        },
+        {
+                icon: 'mbri-website-theme',
+                heading: 'Unlimited Color',
+                text: 'The standart chunk of Lorem Ipsum used since the is reproduced \
         below for those interested'
-    },
-    {
-        icon: 'mbri-growing-chart',
-        heading: 'Media Marketing',
-        text: 'The standart chunk of Lorem Ipsum used since the is reproduced \
+        },
+        {
+                icon: 'mbri-growing-chart',
+                heading: 'Media Marketing',
+                text: 'The standart chunk of Lorem Ipsum used since the is reproduced \
         below for those interested'
-    }
+        }
 
 
 ]
 
 
 const bottomServices = [
-    {
-        icon: 'mbri-wifi',
-        heading: 'Unlimited Wifi',
-        text: 'The standart chunk of Lorem Ipsum used since the is reproduced \
+        {
+                icon: 'mbri-wifi',
+                heading: 'Unlimited Wifi',
+                text: 'The standart chunk of Lorem Ipsum used since the is reproduced \
         below for those interested'
-    },
-    {
-        icon: 'mbri-responsive',
-        heading: 'Responsive Design',
-        text: 'The standart chunk of Lorem Ipsum used since the is reproduced \
+        },
+        {
+                icon: 'mbri-responsive',
+                heading: 'Responsive Design',
+                text: 'The standart chunk of Lorem Ipsum used since the is reproduced \
         below for those interested'
-    },
-    {
-        icon: 'mbri-edit',
-        heading: 'Easy to customize',
-        text: 'The standart chunk of Lorem Ipsum used since the is reproduced \
+        },
+        {
+                icon: 'mbri-edit',
+                heading: 'Easy to customize',
+                text: 'The standart chunk of Lorem Ipsum used since the is reproduced \
         below for those interested'
-    }
+        }
 
 
 ]
