2b31329f7307e55c3aac401b9685e4245380f081
Vadim <vadim.mozeiko@gmail.com>
2021-02-23 12:55:01 +0200

    Multiple images #76

--- js/components/renderBlog.js
+++ js/components/renderBlog.js
@@ -13,6 +13,45 @@ function renderBlog(data) {
     
     for (let i = 0; i < blogData.length; i++){
         const dataItem = blogData[i]
+        
+        if ('video' in dataItem){
+            HTML += ` <div class="box">
+            <div class="box-img">
+            <img src="${dataItem.image}" alt="blog image">
+            <a class="blog-video" href="${dataItem.video}"><i class="fas fa-play"></i></a>
+            </div>
+            <div class="box-text">
+            <h3 class="box-title"><a href="#">${dataItem.title}</a></h3>
+            <p class="box-category"><a href="#">${dataItem.category}</a></p>
+            <p class="box-date">${dataItem.date} <a href="#">${dataItem.author}</a></p>
+            <p class="box-text">${dataItem.article}</p>
+            <a class="read-more" href="#">Read more...</a>
+            </div>
+            </div>`  
+        }
+
+        let img = ''
+        if (Array.isArray(dataItem.image)) {
+            console.log(dataItem.image);
+            for(let i = 0; i < dataItem.image.length; i++){
+                img += `<img class="carousel-img" src="${dataItem.image[i]}" alt="blog image">`
+            }
+            HTML += ` <div class="box">
+            <div class="box-img">
+                ${img}     
+            <a class="slider-prv"><span>&#10094;</span></a>
+            <a class="slider-nxt"><span>&#10095;</span></a>
+            </div>
+            <div class="box-text">
+            <h3 class="box-title"><a href="#">${dataItem.title}</a></h3>
+            <p class="box-category"><a href="#">${dataItem.category}</a></p>
+            <p class="box-date">${dataItem.date} <a href="#">${dataItem.author}</a></p>
+            <p class="box-text">${dataItem.article}</p>
+            <a class="read-more" href="#">Read more...</a>
+            </div>
+            </div>`
+        }
+
         HTML += ` <div class="box">
         <div class="box-img">
         <img src="${dataItem.image}" alt="blog image">
